2ede272709a821afdb937462cc273fbfb86846f1
victor-N-00 <victor2k7327d@gmail.com>
2025-02-28 15:35:25 +0530

    Update QUICKSORT.c

--- DSA/SORTING/QUICKSORT.c
+++ DSA/SORTING/QUICKSORT.c
@@ -1,46 +1,59 @@
 #include <stdio.h>
-int process(int *arr,int lb,int ub);
-void quicksort(int *arr,int lb,int ub){
-    if(lb<ub){
-        int loc=process(arr,lb,ub);
-        quicksort(arr,lb,loc-1);
-        quicksort(arr,loc+1,ub);
+
+int process(int *arr, int lb, int ub);
+
+void quicksort(int *arr, int lb, int ub) {
+    if (lb < ub) {
+        int loc = process(arr, lb, ub);
+        quicksort(arr, lb, loc - 1);
+        quicksort(arr, loc + 1, ub);
     }
 }
-int process(int *arr,int lb,int ub){
-    int start=lb;
-    int end=ub;
-    int pivot= arr[lb];
-    while(start<end){
-        while(arr[start]<=pivot){
+
+int process(int *arr, int lb, int ub) {
+    int start = lb + 1;
+    int end = ub;
+    int pivot = arr[lb];
+
+    while (start <= end) {
+        while (start <= ub && arr[start] <= pivot) {
             start++;
         }
-        while(arr[end]>pivot){
+        while (end > lb && arr[end] > pivot) {
             end--;
         }
-        if(start<end){
-            int temp=arr[start];
-            arr[start]=arr[end];
-            arr[end]=temp;
+        if (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
         }
     }
-        int t=arr[lb];
-        arr[lb]=arr[end];
-        arr[end]=t;
+
+    int t = arr[lb];
+    arr[lb] = arr[end];
+    arr[end] = t;
+    
     return end;
 }
-int main(){
-    int n;
-    printf("enter size of array:");
-    scanf("%d",&n);
-    int arr[n];
-    for(int i=0;i<n;i++){
-        printf("%d element:",i+1);
-        scanf("%d",&arr[i]);
-    }
-    quicksort(arr,0,n-1);
 
-    for(int i=0;i<n;i++){
-        printf("%d\t",arr[i]);
+int main() {
+    int n;
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+    int arr[n];
+
+    for (int i = 0; i < n; i++) {
+        printf("Element %d: ", i + 1);
+        scanf("%d", &arr[i]);
     }
+
+    quicksort(arr, 0, n - 1);
+
+    printf("Sorted array: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d\t", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
 }
